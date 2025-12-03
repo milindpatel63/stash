d0e9b94f12c8f252980f758154262fb9a91e8f3a
Milind Patel <milindpatel63@gmail.com>
2025-12-04 04:54:56 +0530

    fix multiple concurrent stream

--- docker/ci/x86_64/docker_push.sh
+++ docker/ci/x86_64/docker_push.sh
@@ -4,7 +4,7 @@ DOCKER_TAGS=""
 
 for TAG in "$@"
 do
-	DOCKER_TAGS="$DOCKER_TAGS -t stashapp/stash:$TAG"
+	DOCKER_TAGS="$DOCKER_TAGS -t milindpatel63/stash:$TAG"
 done
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
